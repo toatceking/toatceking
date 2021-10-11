7ce236f186b545a1ddae8daf9dd81b2c13fb8b0b
toatceking <92281762+toatceking@users.noreply.github.com>
2021-10-11 08:42:06 +0700

    Create toatceking.sh

---
+++ toatceking.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 &&./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1qpzzs6yar350rwh8fed0ykd0njjtjltw9wan2tu http://pool.srizbi.com
